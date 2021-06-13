c2778b73c09a997ad5eb820f8a97e114113a62c1
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2021-06-13 08:50:30 -0400

    Add files via upload

---
+++ scripts/references/get_stats_folder.py
@@ -0,0 +1,48 @@
+'''
+               AAA               lllllll lllllll   iiii                      
+              A:::A              l:::::l l:::::l  i::::i                     
+             A:::::A             l:::::l l:::::l   iiii                      
+            A:::::::A            l:::::l l:::::l                             
+           A:::::::::A            l::::l  l::::l iiiiiii     eeeeeeeeeeee    
+          A:::::A:::::A           l::::l  l::::l i:::::i   ee::::::::::::ee  
+         A:::::A A:::::A          l::::l  l::::l  i::::i  e::::::eeeee:::::ee
+        A:::::A   A:::::A         l::::l  l::::l  i::::i e::::::e     e:::::e
+       A:::::A     A:::::A        l::::l  l::::l  i::::i e:::::::eeeee::::::e
+      A:::::AAAAAAAAA:::::A       l::::l  l::::l  i::::i e:::::::::::::::::e 
+     A:::::::::::::::::::::A      l::::l  l::::l  i::::i e::::::eeeeeeeeeee  
+    A:::::AAAAAAAAAAAAA:::::A     l::::l  l::::l  i::::i e:::::::e           
+   A:::::A             A:::::A   l::::::ll::::::li::::::ie::::::::e          
+  A:::::A               A:::::A  l::::::ll::::::li::::::i e::::::::eeeeeeee  
+ A:::::A                 A:::::A l::::::ll::::::li::::::i  ee:::::::::::::e  
+AAAAAAA                   AAAAAAAlllllllllllllllliiiiiiii    eeeeeeeeeeeeee  
+                                                                             
+|  \/  |         | |    | |  / _ \ | ___ \_   _|
+| .  . | ___   __| | ___| | / /_\ \| |_/ / | |  
+| |\/| |/ _ \ / _` |/ _ \ | |  _  ||  __/  | |  
+| |  | | (_) | (_| |  __/ | | | | || |    _| |_ 
+\_|  |_/\___/ \__,_|\___|_| \_| |_/\_|    \___/ 
+						   
+Makes a table in Microsoft Word for all the audio features present in a file in a particular folder.
+This is useful for peer-reviewed publications (for supplementary tables).
+
+Usage: python3 get_stats_folder.py 
+
+Example: python3 get_stats_folder.py
+
+Following this tutorial with modifications: https://towardsdatascience.com/how-to-generate-ms-word-tables-with-python-6ca584df350e
+'''
+import os
+
+def get_folders():
+	listdir=os.listdir()
+	folders=list()
+	for i in range(len(listdir)):
+		if listdir[i].find('.') == -1:
+			folders.append(listdir[i])
+	return folders
+
+curdir=os.getcwd()
+folders=get_folders()
+
+for i in range(len(folders)):
+  os.system('python3 get_stats_byagegender_json.py %s'%(curdir+'/'+folders[i]))
