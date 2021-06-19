52cbc7eb30557c2fa94c4e675696212148fc015d
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2021-06-19 15:44:39 -0400

    Update sample_analysis.py

--- sample_analysis.py
+++ sample_analysis.py
@@ -40,7 +40,7 @@ Thank you for your interest in our work!
                 LICENSE TERMS                      
 ================================================ 
 
-Copyright 2021 NeuroLex Laboratories, Inc. 
+Copyright 2021 by Jim Schwoebel.
 Licensed under the Apache License, Version 2.0 (the "License"); 
 you may not use this file except in compliance with the License. 
 You may obtain a copy of the License at 
