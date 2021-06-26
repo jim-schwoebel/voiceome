1e2328649e690f503a4ec37ee9225231b041abd9
Jim Schwoebel <30424731+jim-schwoebel@users.noreply.github.com>
2021-06-25 21:25:49 -0400

    Update test.py

--- test.py
+++ test.py
@@ -354,7 +354,7 @@ class test_visualization(unittest.TestCase):
             if listdir[i].endswith('.png'):
                 os.remove(listdir[i])
         os.chdir(curdir)
-        os.system('python3 cli.py --command visualize --agegender FourtiesMale] --vtype bar --agegender ThirtiesMale --verbosity False')
+        os.system('python3 cli.py --command visualize --agegender FourtiesMale --vtype bar --agegender ThirtiesMale --verbosity False')
         os.chdir(visual_dir)
         listdir=os.listdir()
         if 'OpensmileFeatures_F0semitoneFrom27.5Hz_sma3nz_amean.png' in listdir:
@@ -375,7 +375,7 @@ class test_visualization(unittest.TestCase):
             if listdir[i].endswith('.png'):
                 os.remove(listdir[i])
         os.chdir(curdir)
-        os.system('python3 cli.py --command visualize --agegender FourtiesMale] --vtype bar_cohorts --agegender ThirtiesMale --agegender FiftiesMale --verbosity False')
+        os.system('python3 cli.py --command visualize --agegender FourtiesMale --vtype bar_cohorts --agegender ThirtiesMale --agegender FiftiesMale --verbosity False')
         os.chdir(visual_dir)
         listdir=os.listdir()
         if 'OpensmileFeatures_F0semitoneFrom27.5Hz_sma3nz_amean_FourtiesMale]_ThirtiesMale.png' in listdir:
